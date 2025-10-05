aedda0146007342d94e4351a6c0556304fa2e38d
Camryn <142862831+CamrynOS@users.noreply.github.com>
2025-10-04 21:35:34 -0400

    added basic status and top part to the change schedule page

--- dev-strength-react/src/Pages/ChangeSchedule/ChangeSchedule.jsx
+++ dev-strength-react/src/Pages/ChangeSchedule/ChangeSchedule.jsx
@@ -1,6 +1,26 @@
+import styles from './ChangeSchedule.module.css'
+import { useState } from 'react';
+
 function ChangeSchedule() {
+    const [isEditing, setIsEditing] = useState(false);
+
     return(
-        <h1>Change Schedule</h1>
+        <main>
+            <h1>Schedule</h1>
+            <div className={styles.subheader}>
+                <h2>{isEditing ? "Edit Schedule" : "Current Schedule"}</h2>
+                <div>
+                    {!isEditing ? (
+                        <button className={styles.editButton} onClick={() => setIsEditing(true)}>Edit Schedule</button>
+                    ) : (
+                        <div>
+                            <button className={styles.saveButton}>Save Changes</button>
+                            <button className={styles.cancelButton} onClick={() => setIsEditing(false)}>Cancel</button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </main>
     );
 }
 
